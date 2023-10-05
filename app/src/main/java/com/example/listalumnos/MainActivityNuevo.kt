ee13dcdbf623bd2b082292770d751ea9285faa50
AlexCernas2901 <acernas1@ucol.mx>
2023-10-04 23:53:15 -0600

    first commit

---
+++ app/src/main/java/com/example/listalumnos/MainActivityNuevo.kt
@@ -0,0 +1,11 @@
+package com.example.listalumnos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityNuevo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_nuevo)
+    }
+}
